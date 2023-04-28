b855586c45354b66e04bf9eb90a2d27c604cfdfd
Gu4x1 <45929092+Gu4x1@users.noreply.github.com>
2023-04-28 00:11:54 -0300

    Update filter-nuclei.sh
    removing http directory

--- filter-nuclei.sh
+++ filter-nuclei.sh
@@ -2,7 +2,12 @@
 
 cd ~/Downloads
 git clone https://github.com/projectdiscovery/nuclei-templates
-find nuclei-templates/ -name "*.yaml" > /tmp/1.txt
+cd nuclei-templates/
+cp -r http/* .
+rm -rf http/
+cd ..
+
+find nuclei-templates/ -name "*.yaml" | sed 's/http\///'  > /tmp/1.txt
 cd ~
 find nuclei-templates/ -name "*.yaml" > /tmp/2.txt
 
@@ -10,6 +15,6 @@ diff /tmp/1.txt /tmp/2.txt |grep yaml | cut -d " " -f2-23  > /tmp/3.txt
 
 mkdir ~/Desktop/templatenuclei/
 for i in $(cat /tmp/3.txt);do cp ~/Downloads/$i ~/Desktop/templatenuclei/ ;done
-clear  
-echo "The new Templates at ~/Desktop/templatenuclei/"
+clear
+echo "Salvos em ~/Desktop/templatenuclei/"
 rm -rf ~/Downloads/nuclei-templates/
