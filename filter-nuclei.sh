0e30ab77af7ce43681de9b034d1fc0fff505fe7e
Gu4x1 <45929092+Gu4x1@users.noreply.github.com>
2023-08-04 18:42:25 -0300

    Update filter-nuclei.sh

--- filter-nuclei.sh
+++ filter-nuclei.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+# tool para filtrar templates novos antes da atualização do nuclei
+
 cd ~/Downloads
 git clone https://github.com/projectdiscovery/nuclei-templates
-cd nuclei-templates/
-cp -r http/* .
-rm -rf http/
-cd ..
+cd ~/
 
-find nuclei-templates/ -name "*.yaml" | sed 's/http\///'  > /tmp/1.txt
-cd ~
+find nuclei-templates/ -name "*.yaml"   > /tmp/1.txt
+cd ~/Downloads/
 find nuclei-templates/ -name "*.yaml" > /tmp/2.txt
 
 diff /tmp/1.txt /tmp/2.txt |grep yaml | cut -d " " -f2-23  > /tmp/3.txt
